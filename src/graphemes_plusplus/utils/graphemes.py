d7626ff9d321d9eca8f7e4832e43335ce7525ae7
vmenan <themachinequest@gmail.com>
2026-03-07 20:48:46 +0100

    removed init from GraphemeSplitter

--- src/graphemes_plusplus/utils/graphemes.py
+++ src/graphemes_plusplus/utils/graphemes.py
@@ -1,5 +1,3 @@
-import string
-
 import grapheme
 
 
@@ -9,9 +7,6 @@ class GraphemeSplitter:
     Standardized Grapheme Clustering class.
     Extends 'grapheme' library to handle "க்ஷ", "ஸ்ரீ", "ஶ்ரீ" and sinhala ZWJ.
     """
-    def __init__(self, grapheme_backbone: str = "grapheme") -> None:
-       print(f"Intialized")
-
     @staticmethod
     def split(string: str) -> list[str]:
         """
